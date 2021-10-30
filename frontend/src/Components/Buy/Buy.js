6cd1031718af2ca56da2bf39eb1c4aea67bc7e92
Ishika Tiwari <ishikatiwari416@gmail.com>
2021-10-30 23:33:32 +0530

    Changes in User interface

--- frontend/src/Components/Buy/Buy.js
+++ frontend/src/Components/Buy/Buy.js
@@ -13,21 +13,39 @@ function Buy(){
              <div className="col-xs-12 col-sm-3 col-md-3 col-lg-4 col-xl-4">
                <ul className="List">
                  <h4> Filters </h4>
-                 <form type="checkbox"> </form>  <div>     PG        </div>
-                 <div>         Hostel     </div>
-                 <div>         Price      </div>
-                 <div>        2000       </div>
-                 <div>        5000       </div>
-                 <div>        8000       </div>
-                 <div>         Girls      </div>
-                 <div>         Boys       </div>
-                 <div>         Veg        </div>
-                 <div>         Non-Veg    </div>
-                 <div>         Both        </div>
-                 <div>      Single-Seated      </div>
-                 <div>      Double-Seated      </div>  
+
+                <div className="filter-spacing">
+                    <div>          PG        </div> 
+                    <div>         Hostel     </div>
+                 </div>
+
+                 <div className="filter-spacing">
+                    <div>         Price      </div>
+                    <div>        2000       </div>
+                    <div>        5000       </div>
+                    <div>        8000       </div>
+                 </div>
+
+                 <div className="filter-spacing">
+                     <div>         Girls      </div>
+                     <div>         Boys       </div>
+                 </div>
+
+                 <div className="filter-spacing">
+                     <div>         Veg        </div>
+                     <div>         Non-Veg    </div>
+                     <div>         Both        </div>
+                 </div>
+
+                 <div>
+                     <div>      Single-Seated      </div>
+                     <div>      Double-Seated      </div> 
+                 </div>
+
                </ul>
              </div>
+
+
              <div className="row row-list">
 
                <div className="Room-row">
@@ -52,6 +70,7 @@ function Buy(){
                        <div>Address  
                          dkcnkadjchbvgdcsjbhnask xmasxlaśxmksjabchjvdc bdn cm
                        </div>
+                       
                 </div>
                </div>
 
--- frontend/src/Components/Sell/Sell.js
+++ frontend/src/Components/Sell/Sell.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import room1 from './../../assets/room1.png'
 
 function Sell(){
     return(
 
         <div>
 
+            <div className="row">
+                <div className="col-sm-3 col-lg-4 col-lg-4">
+                    <img src={room1}></img>
+                </div>
+
+                <div>
+
+                    <div>
+                        Details
+                    </div>
+
+                </div>
+            </div>
         </div>
 
     );
