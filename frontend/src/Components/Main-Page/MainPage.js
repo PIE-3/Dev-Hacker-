880bcbb53b5af312dbe39a45e266b0c15ad852e0
Sheetal575 <sheetaldadh9@gmail.com>
2021-10-30 14:25:04 +0530

    main page is done

--- frontend/src/Components/Main-Page/MainPage.js
+++ frontend/src/Components/Main-Page/MainPage.js
@@ -1,28 +1,36 @@
-
-
 import React from "react";
 import Background from "./../../assets/Background.png";
-
+import front from './../../assets/undraw.png';
+import './MainPage.css';
 
 function MainPage(){
     return(
-        <div>
-            <div className="row">
-                <div className="row bg-image">
-                    <img src= {Background} ></img>
-
-                    <div className="row">
-                    <div> 
-                        <button className="button-fill">Buy</button> 
-                    </div>
-                    <div> 
-                        <button className="button-fill">Rent</button> 
-                    </div>
+        <div className="row front-container p-5 w-100 align-items-center">
+            <div className="col-lg-6 col-sm-12 text-center">
+                <img  src={front} alt="" />
+            </div>
+            <div className="col-lg-6 col-sm-12  ">
+                <div className="row h-25 text-center align-items-start project-name">                    
+                    <h1 >PROJECT NAME</h1>
                 </div>
+                <div className="row h-25 text-center align-items-end">                    
+                    <h2 className="text-secondary">Find the Best Pg, Hostel for you</h2>
+                </div>
+                <br />
+                <br />
+
+                <div className="row w-100 text-center align-items-start g-3">
+                    <div className="col-lg-6 col-sm-12">
+                        <button className="btn search-button rounded-pill">Search </button>
+                    </div>
+                    <div className="col-lg-6 cik-sm-12">
+                        <button className="btn add-button rounded-pill">Add</button>
+                    </div>
                 </div>
             </div>
-
+           
         </div>
+
     );
 }
 
